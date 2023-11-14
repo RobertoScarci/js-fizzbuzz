1bc23ddf82782f3cf04dea7567c180737dff2d2f
RobertoScarci <robertoscarci.02@gmail.com>
2023-11-14 14:11:42 +0100

    impostazione del cycle for per sezionare i 100 numeri

--- js/scritp.js
+++ js/scritp.js
@@ -1,1 +1,15 @@
 // individuo le variabili da inserire nell'html
+const listElement = document.querySelector('div.list')
+
+// faccio l'elenco dei 100 numeri nella pagina
+for( let i = 1; i <= 100; i++){
+    const cardElement = document.createElement('h3')
+    cardElement.innerHTML = i
+    listElement.appendChild(cardElement)
+
+    if( i % 3 == 0){
+        cardElement.innerHTML = 'Fizz'
+    } if( i % 5 == 0){
+        cardElement.innerHTML = 'Buzz'
+    }
+}
